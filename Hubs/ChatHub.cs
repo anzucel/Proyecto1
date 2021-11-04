4555933ada14d8a41980a16e7afc9ff973490163
Zucely Hernandez <andreazucely70@gmail.com>
2021-11-03 22:47:07 -0600

    Agregar archivos de proyecto.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string Username, string Message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Username, Message);
+        }
+    }
+}
